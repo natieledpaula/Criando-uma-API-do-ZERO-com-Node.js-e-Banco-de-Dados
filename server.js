08bfcdc740a067af5325c1966fe4dae075b0e1d1
natieledpaula <natiele887@gmail.com>
2025-05-14 18:58:24 -0300

    Criar usuario

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
 import express from 'express'
 
+const app = express()
+const users = []
+
+app.post('/usuarios', (req, res) => {
+    
+})
+
+app.get('/usuarios', (req, res) => {
+    res.send('Ok, deu bom')
+})
+
+app.listen(3000)
+
+/* 
+    Criar nossa API de Usuários
+    - Criar um Usuário
+    - Listar todos os Usuários
+    - Editar um Usuário
+    - Deletar um Usuário
+*/
+
+/* 
+1) Tipo de Rota / Mettodo HTTP
+2) Endereço
+*/
